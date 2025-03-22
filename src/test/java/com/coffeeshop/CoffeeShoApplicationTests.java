aa14bea88e2cebb1407c2dec8568755721c9059b
Мирон Москаленко <mironmoskalenko2000@gmail.com>
2025-03-22 23:14:15 +0600

    Fixed bugs and now working with DockerDesktop but cant do bootRun

--- src/test/java/com/coffeeshop/CoffeeShoApplicationTests.java
+++ src/test/java/com/coffeeshop/CoffeeShoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class CoffeeShopBackendApplicationTests {
+class CoffeeShoApplicationTests {
 
 	@Test
 	void contextLoads() {
